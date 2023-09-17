d0937f082469a79efb2bad9e883afd4f2691f13c
aleksandr2639 <alexanderkriulin@yandex.ru>
2023-09-17 21:19:10 +0300

    add:project

---
+++ src/js/isValidateData.js
@@ -0,0 +1,12 @@
+export default class Validator {
+    constructor(name, number) {
+        this.name = name;
+        this.number = number;
+    }
+    validateUsername(name) {
+
+    }
+    validateNumber(name) {
+
+    }
+}
